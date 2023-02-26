cc6523f8995640b406672c94ed25aa52e4187eb1
B_Cuvelier <lejoker82@gmail.com>
2023-02-26 06:48:44 +0100

    Ajouts méthodes pour Deck(). Changement de nom de classe.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import random
+import random as rng
 
 
-class Cartes:
+class Deck:
     def __init__(self):
-        self.color = ["Carreau", "Pique", "Coeur", "Tréfle"]
-        self.number = ["As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi"]
+        self.colors = ["Carreau", "Pique", "Coeur", "Tréfle"]
+        self.numbers = ["As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi"]
+        self.paquet = []
+        self.nb_cards = 0
+        self.creer_paquet()
+        self.riffle_riffle()
 
-    def creer_carte(self):
-        return random.choice(self.color), random.choice(self.number)
+    def creer_paquet(self):
+        for color in self.colors:
+            for number in self.numbers:
+                self.paquet.append((color, number))
+
+    def riffle_riffle(self):
+        rng.shuffle(self.paquet)
+
+    def draw_random_card(self):
+        rand_index = rng.randint(0, 51)
+        return self.paquet.pop(rand_index)
+
+
+paquet = Deck()
+print(paquet.paquet)
