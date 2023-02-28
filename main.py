2738e05508c96754434f8b472eaeb76b07630706
B_Cuvelier <lejoker82@gmail.com>
2023-02-28 03:55:32 +0100

    Restucturation de la class Deck

--- main.py
+++ main.py
@@ -2,43 +2,40 @@ import random as rng
 
 
 class Deck:
-    def __init__(self):
-        self.colors = ["Carreau", "Pique", "Coeur", "Tréfle"]
+    def __init__(self, nb_deck=1):
+        self.colors = ["Carreau", "Pique", "Coeur", "Trèfle"]
         self.numbers = ["As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi"]
-        self.paquet = []
-        self.set_deck()
+        self.cards = []
+        self.set_deck(nb_deck)
         self.riffle_riffle()
 
-    def set_deck(self):
-        for color in self.colors:
-            for number in self.numbers:
-                self.paquet.append((number, color))
+    def set_deck(self, nb_deck):
+        for nb in range(nb_deck):
+            for color in self.colors:
+                for number in self.numbers:
+                    self.cards.append((number, color))
 
     def riffle_riffle(self):
-        rng.shuffle(self.paquet)
+        rng.shuffle(self.cards)
 
-    def draw_card(self):
-        return self.paquet.pop()
+    def draw_first_card(self):
+        return self.cards.pop(0)
 
     def reset_deck(self):
-        self.paquet[:] = []
+        self.cards[:] = []
         self.set_deck()
         self.riffle_riffle()
-        return print("*Riffle Riffle* Paquet reconstitué et mélangé")
+        return print("*Riffle Riffle* Deck reconstitué et mélangé")
 
     def draw_board(self):
-        self.draw_card()
-        flop = [self.draw_card(), self.draw_card(), self.draw_card()]
-        self.draw_card()
-        turn = self.draw_card()
-        self.draw_card()
-        river = self.draw_card()
+        self.draw_first_card()  # 1st burnt card
+        flop = [self.draw_first_card(), self.draw_first_card(), self.draw_first_card()]
+        self.draw_first_card()  # 2nd burnt card
+        turn = self.draw_first_card()
+        self.draw_first_card()  # 3rd burnt card
+        river = self.draw_first_card()
         return flop + [turn, river]
 
 
-deck = Deck()
-j1 = [deck.draw_card(), deck.draw_card()]
-j2 = [deck.draw_card(), deck.draw_card()]
-j3 = [deck.draw_card(), deck.draw_card()]
-board = deck.draw_board()
-print(board)
+deck = Deck(6)
+print(deck.cards)
